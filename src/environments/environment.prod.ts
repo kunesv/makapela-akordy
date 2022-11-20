f0cb0df239ccb5a0283ef508b8c661d014a78fad
kunesv <kunesv@sprinters.cz>
2022-11-20 21:23:16 +0100

    Adding Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'makapelaakordy',
+    appId: '1:689062887655:web:779dd17dd8eefd7e67e242',
+    databaseURL: 'https://makapelaakordy-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'makapelaakordy.appspot.com',
+    apiKey: 'AIzaSyBUA4XldNEF-TCBX8qa5OysoNe9vPebPOQ',
+    authDomain: 'makapelaakordy.firebaseapp.com',
+    messagingSenderId: '689062887655',
+    measurementId: 'G-3YE2L7BD8B',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'makapelaakordy',
+    appId: '1:689062887655:web:779dd17dd8eefd7e67e242',
+    databaseURL: 'https://makapelaakordy-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'makapelaakordy.appspot.com',
+    apiKey: 'AIzaSyBUA4XldNEF-TCBX8qa5OysoNe9vPebPOQ',
+    authDomain: 'makapelaakordy.firebaseapp.com',
+    messagingSenderId: '689062887655',
+    measurementId: 'G-3YE2L7BD8B',
+  },
   production: false
 };
 
